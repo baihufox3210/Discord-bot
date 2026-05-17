3d4c0d4cbb48b9d6b9a3c3fe1d76f0a84d35ada8
baihu Jetson-Orin <baihu3210@gmail.com>
2026-05-17 18:06:54 +0800

    fix: update logger initialization to use named parameters for clarity

--- core/logger.py
+++ core/logger.py
@@ -1,5 +1,7 @@
 from discord_logger import DiscordLogger
 
 logger = DiscordLogger(
-    "https://discord.com/api/webhooks/1505027277585383427/V-qpxEu7_kwoFElo5ir-Jg76ZmN94Eo-SUT0d1A6d9n2iktHgCrI64dJfeNq6ck5X_lC"
+    webhook_url="https://discord.com/api/webhooks/1505027277585383427/V-qpxEu7_kwoFElo5ir-Jg76ZmN94Eo-SUT0d1A6d9n2iktHgCrI64dJfeNq6ck5X_lC",
+    owner_avatar="https://github.com/baihufox3210/abnormal/blob/main/owner_avatar.jpg?raw=true",
+    webhook_avatar="https://github.com/baihufox3210/abnormal/blob/main/webhook_avatar.webp?raw=true"
 )
